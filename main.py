a6ab44a71c8e765701531c74637f73e938fe74f3
Lewis Jang <lewis.aedes@gmail.com>
2023-01-03 19:44:04 +0800

    rewrite most part, make split all build by array, and read write by librosa.

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ import os
 import glob
 
 sampling_rate = 48000
+saving_datatype = "float32"
 output_dir = "split_audio"
 noise_folder = "sample"
-only_export_full_version = True
+cache_folder = "cache"
+rebuild_cache = False
+only_export_full_version = False
 
 def calculate_similarity(wave1, wave2):
     wave1 = np.array(wave1)
@@ -29,8 +32,6 @@ def calculate_similarity(wave1, wave2):
         print(e)
         return 0
 
-    print("fft working")
-
     correlation = np.corrcoef(fft1, fft2)[0][1]
 
     return correlation
@@ -45,69 +46,30 @@ def audio_format(audio):
 def audio_format_array(audio):
     return audio_format(audio).get_array_of_samples()
 
-def array_to_audio_segment(array, sample_rate=44100, sample_width=2):
-    # Convert the array to a NumPy array if necessary
-    if not isinstance(array, np.ndarray):
-        array = np.array(array)
-
-    if array.shape[0] == 0:
-        return AudioSegment.silent(duration=1)
-
-    # Add a channel dimension if the array is one-dimensional
-    if array.ndim == 1:
-        array = array[:, np.newaxis]
-
-    # Normalize the array values to the range [-1, 1]
-    array = array / np.max(np.abs(array))
-
-    # Convert the array to a bytes object
-    array = (array * 2**15).astype(np.int16)
-    audio_bytes = array.tobytes()
-
-    # Create the AudioSegment object
-    audio_segment = AudioSegment(
-        audio_bytes, 
-        frame_rate=sample_rate, 
-        sample_width=sample_width,
-        channels=array.shape[1]
-    )
-
-    return audio_segment
-
-def add_silent(audio):
-    # 0.1 seconds in milliseconds
-    pause_duration = 0.3 * 1000
-    pause = AudioSegment.silent(duration=pause_duration)
-
-    return audio + pause
-
-def split_audio_by_noise(audio, output_dir):
+def split_audio_by_noise(target_audio_path, output_dir):
     # Create output directories if they don't already exist
     if not os.path.exists(output_dir + "/noise"):
         os.makedirs(output_dir + "/noise")
     if not os.path.exists(output_dir + "/clean"):
         os.makedirs(output_dir + "/clean")
 
-    new_audio = AudioSegment.silent(duration=1)
-
-    segment_length = None
+    new_audio = []
     noise_samples = []
-    audio_data = audio_format_array(audio)
+    segment_length = None
+    audio_data, _ = load_wav(os.path.join(cache_folder, target_audio_path))
 
-    for i in glob.glob(os.path.join(".", noise_folder, "*.wav")):
-        noise_data = audio_format(AudioSegment.from_file(i))
-        noise_data = add_silent(noise_data)
-        noise_data = audio_format_array(noise_data)
+    for i in glob.glob(os.path.join(".", cache_folder, noise_folder, "*.wav")):
+        noise_data, _ = load_wav(i)
         noise_samples.append(noise_data)
 
         size = len(noise_data)
 
-    if segment_length is None or size < segment_length:
-        segment_length = size
+        if segment_length is None or size < segment_length:
+            segment_length = size
 
     for i in range(0, len(audio_data), segment_length):
         start_time = i
-        end_time = start_time + segment_length - 1
+        end_time = start_time + segment_length
         segment = audio_data[start_time:end_time]
 
         noise_present = False
@@ -116,18 +78,54 @@ def split_audio_by_noise(audio, output_dir):
                 noise_present = True
                 break
 
-        segment = array_to_audio_segment(segment, audio.frame_rate, audio.sample_width)
-
         if not only_export_full_version:
             if noise_present:
-                segment.export(os.path.join(output_dir, "noise", "segment_{}.wav".format(i)), format="wav")
+                write_wav(os.path.join(output_dir, "noise", "segment_{}.wav".format(i)), segment)
             else:
-                segment.export(os.path.join(output_dir, "clean", "segment_{}.wav".format(i)), format="wav")
+                write_wav(os.path.join(output_dir, "clean", "segment_{}.wav".format(i)), segment)
 
         if not noise_present:
-            new_audio = new_audio + audio[start_time:start_time + segment_length]
+            if isinstance(segment, np.ndarray):
+                segment = segment.tolist()
 
-    new_audio.export(os.path.join(output_dir, "clean", "clean_version.wav".format(i)), format="wav")
+            if isinstance(new_audio, np.ndarray):
+                new_audio = new_audio.tolist()
 
-audio = AudioSegment.from_file("test.wav")
-split_audio_by_noise(audio, output_dir)
+            new_audio = new_audio + segment
+    
+    write_wav(os.path.join(output_dir, "clean", "clean_version.wav"), new_audio)
+
+def load_wav(path):
+    return librosa.load(path, sr=None)
+
+def write_wav(path, data):
+    if not isinstance(data, np.ndarray):
+        data = np.array(data)
+
+    librosa.output.write_wav(path, data.astype(saving_datatype), sampling_rate)
+
+def build_cache(target_path):
+    audio = AudioSegment.from_file(target_path)
+    name = os.path.basename(target_path)
+    folders = [cache_folder] + os.path.dirname(target_path).split(os.sep)
+
+    for level, dir_name in enumerate(folders):
+        if dir_name == ".":
+            continue
+
+        path = os.path.join(*folders[0:level + 1])
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    new_path = os.path.join(*folders, name)
+
+    audio_format(audio).export(new_path, format="wav")
+
+target_audio_path = "test.wav"
+
+if rebuild_cache:
+    for i in glob.glob(os.path.join(".", noise_folder, "*.wav")) + [target_audio_path]:
+        build_cache(i)
+
+split_audio_by_noise(target_audio_path, output_dir)
